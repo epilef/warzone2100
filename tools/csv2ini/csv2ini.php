36b23ff571ece66cbcc73ddd4ee5fb59bab3fef9
felippico <sailboat.joaquina@gmail.com>
2012-11-14 00:58:37 -0200

    lot of fixes

--- tools/csv2ini/csv2ini.php
+++ tools/csv2ini/csv2ini.php
@@ -9,7 +9,7 @@ if ($argc == 2)
 	$arr1 = explode(",", $lines[1]);
 	$arr2 = explode(",", $lines[2]);
 	$multivalue = false;
-	if ($arr1[0] == $arr1[1]) {$multivalue = true;}
+	if ($arr1[0] == $arr2[0]) {$multivalue = true;}
 	if (!$multivalue) 
 	{
 		for($i=1;$i<$c_lines;$i++) {
@@ -27,11 +27,16 @@ if ($argc == 2)
 		$lastsection = "";
 		// $section[] = "";
 		$fpas = true;
+		$x=0;
 		for($i=1;$i<$c_lines;$i++) {
 			$arrayd = explode(",", $lines[$i]);
 			$c_arrayd = count($arrayd);
 			if ( $lastsection != $arrayd[0] ) {
-				if ($section) foreach ($section as $iniline) $data .= $iniline."\n";
+				if ($section) {
+					foreach ($section as $iniline) 
+						$data .= $iniline."\"\n";
+					$data .= "\n";	
+				}
 				$data.="[".$arrayd[0]."]\n";
 				$lastsection = $arrayd[0];
 				unset($section);
@@ -43,21 +48,24 @@ if ($argc == 2)
                                         { $field = "\"".$field."\""; }
 				$field = trim($field, "\n");
 				if ($fpas)  
-					$section[$x] = $array01[$x]." = ".$field;
+					$section[$x] = $array01[$x]." = \"".$field;
 				else
 					$section[$x] .= ",".$field;
-					
 			}
 			$fpas = false;
 		}	
-		foreach ($section as $iniline) $data .= $iniline."\n";
+		foreach ($section as $iniline) $data .= $iniline."\"\n";
 	}
- 	echo $data;
+ 	echo "$data\n";
 }
 else
 {
 	echo "\n";
 	echo "Usage: php csv2ini.php file.csv > file.ini\n";
+	echo "\n";
+	echo "Notes: before converting you have to create and/or adjust all the first descriptive row\n";
+	echo "Spaces are not allowed !\n";
+	echo "probably you need to do something like: php csv2ini.php file.csv | grep -v \"nused\" > file.ini\n";
 	echo "\n";
 }
 ?>
